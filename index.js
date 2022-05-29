195c65d86fb8eae0f5bfafc644ff09d41a8708c1
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-29 20:18:13 -0300

    connect mongoDb

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8000;
 
+//
+const db = require('./db/connection');
+
+
 // Template Engine
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Importacao de rotas
 const notesRoutes = require('./routes/notes');
@@ -22,6 +27,13 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(PORT, () => {
-    console.log(`Projeto rodando na PORT ${PORT}`);
-})
+db.initDb((err, db) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("O banco conectou")
+        app.listen(PORT, () => {
+            console.log(`Projeto rodando na PORT ${PORT}`);
+    });
+    }
+});
