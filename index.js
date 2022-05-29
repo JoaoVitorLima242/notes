c6b9568b17a1eaed057144658d21501a9ab57429
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-29 15:36:43 -0300

    get in home

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8000;
 
+// Rotas
+app.get('/', (req, res) => {
+    res.send('App esta rodando');
+})
+
 app.listen(PORT, () => {
     console.log(`Projeto rodando na PORT ${PORT}`);
 })
