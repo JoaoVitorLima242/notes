2772ce927427584457d2adec65e3cc39dedf881f
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-29 20:28:27 -0300

    insert notes in Mongodb

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,27 @@
 const Router = require('express').Router;
+const db = require('../db/connection');
+const { ObjectId } = require('mongodb');
+
 const router = Router()
 
 router.get('/', (req, res) => {
     res.render('notes/create')
 });
+
+router.post('/', (req, res) => {
+
+    const {
+        title,
+        description
+    } = req.body;
+
+    db.getDb()
+    .db()
+    .collection('notes')
+    .insertOne({title: title, desc: description})
+
+  res.redirect('/', 301);
+    
+})
 
 module.exports = router;
